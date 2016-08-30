929921ef80ff5f8362db1ace59a894c02ec42eb4
Vrigz Alejo <nuedu2012@gmail.com>
2016-08-30 11:39:25 +0800

    commented and refactored a little bit

--- src/Controller/GithubController.php
+++ src/Controller/GithubController.php
@@ -17,23 +17,34 @@ use App\Traits\Exporter;
 use Goutte\Client;
 use ProgressBar\Manager;
 
+/**
+ * Class GithubController
+ * @package App\Controller
+ */
 class GithubController extends WebScraperController {
 
   use Exporter;
 
+  /**
+   * @param \Goutte\Client $client
+   * @param array          $options contains the argv without dash
+   */
   public function index(Client $client, $options = []) {
 
     $this->options = $options;
     $this->time =  \DateTime::createFromFormat('U', time())->format('U');
-    $crawler = $client->request(HttpMethod::GET, BaseUrl::GITHUB_PROJECTS);
+    $crawler = $client->request(HttpMethod::GET, BaseUrl::GITHUB_PROJECTS_THAT_POWER_GITHUB);
+
+    // Preparing for crawl
     $repoLists = $crawler->filter('ul.repo-list > li.repo-list-item');
+
+    // New instance of \ProgressBar\Manager
     $this->setManager(new Manager(0, count($repoLists)));
     $this->getManager()->setFormat('%current%/%max% [%bar%] %percent%%');
     $this->setCount(0);
-    $crawler->filter('ul.repo-list > li.repo-list-item')->each(function ($node) {
-//      print_r($node->filter( 'a' )->extract( array( 'href' ) ));
-//      print(preg_replace('/[\s\n]/', '', $node->filter( '.repo-list-name a' )->text()));
-//      print(trim($node->filter( '.repo-list-description' )->text()));
+
+    // Loop and crawl!!!
+    $repoLists->each(function ($node) {
 
       $extractUrls = $node->filter( 'a' )->extract( array( 'href' )  );
       foreach($extractUrls as $url) {
@@ -48,18 +59,23 @@ class GithubController extends WebScraperController {
 
       $entityManager = ScrapingEntityManager::getEntityManager();
 
+      // Create a new model
       $webScraper = new WebScraper();
       $webScraper->setData(serialize($this->data));
       $webScraper->setIpAddress(gethostbyname(preg_replace('/^https?:\/\//', '', BaseUrl::GITHUB)));
-      $webScraper->setUrl(BaseUrl::GITHUB_PROJECTS);
+      $webScraper->setUrl(BaseUrl::GITHUB_PROJECTS_THAT_POWER_GITHUB);
       $webScraper->setCreated();
+
+      // Save
       $entityManager->persist($webScraper);
       $entityManager->flush();
 
       $this->getManager()->update($this->getCount() + 1);
+      // Removed the delays in execution
       sleep(0);
       $this->setCount($this->getCount() + 1);
 
+      // Setting the export path
       $exportPathfile = (isset($this->options['e'])) ? $this->options['e']
         : ((isset($this->options['export'])) ? $this->options['export'] : null);
 
@@ -69,6 +85,7 @@ class GithubController extends WebScraperController {
         $this->export($this->data, "github-{$this->time}.tsv");
       }
 
+      // void
       print_r($this->data);
     });
   }
