b860edeffc007af7ebc3e2c26c3ab0f6b0e2b637
Vrigz Alejo <nuedu2012@gmail.com>
2016-08-30 11:40:37 +0800

    added comments Exporter

--- src/Traits/Exporter.php
+++ src/Traits/Exporter.php
@@ -14,7 +14,14 @@ trait Exporter {
   private $file = 'file.tsv';
 
 
-  public function export($data, $filename = null, $dir = null, $delimeter = "\t", $mode = 'a') {
+  /**
+   * @param array  $data
+   * @param null   $filename
+   * @param null   $dir
+   * @param string $delimeter defaults to tab
+   * @param string $mode defaults to 'a' append
+   */
+  public function export($data = [], $filename = null, $dir = null, $delimeter = "\t", $mode = 'a') {
 
     if(is_null($filename) && is_null($dir)) {
       $filepath = $this->baseDataDirectory() . DIRECTORY_SEPARATOR . $this->file;
@@ -29,6 +36,9 @@ trait Exporter {
     fclose($fp);
   }
 
+  /**
+   * @return string
+   */
   public function baseDataDirectory() {
     $defaultDataDirectory = dirname( __FILE__ ) . '/../data';
 
